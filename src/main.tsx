37ec943e98157487ee0db00f2b4c16050b4756b7
Antonije Ljubisa <ogistdipen@outlook.com>
2024-03-02 10:17:21 +0100

    add tailwind add and run initial test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <p>Homepage</p>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </React.StrictMode>
 );
