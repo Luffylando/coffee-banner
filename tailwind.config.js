0085f55c0ed5cccc2983d51ac69b325a36d0f379
Antonije Ljubisa <ogistdipen@outlook.com>
2024-03-04 19:43:35 +0100

    add eslint ptettier deps tailwind ts config etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                grey: "#424243",
+                bluePrimary: "#34A0DB",
+                blueSecondary: "#B8E3F4",
+                greySecondary: "#CECECE",
+                greyTertiary: "#767676",
+                greyQuaternary: "#F3F3F3",
+                greyBorder: "#D4D4D4",
+                white: "#FFFFFF"
+            },
+            fontFamily: {
+                montserrat: "Montserrat"
+            },
+            boxShadow: {
+                cardShadow: "0px 1px 40px 0px #42424321"
+            }
+        }
+    },
+    plugins: []
 };
